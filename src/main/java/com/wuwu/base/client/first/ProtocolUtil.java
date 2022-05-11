c9f95ad1cce695d42bb8e89134b332fcd8737283
makaikai <makaikai@accesscorporate.com.cn>
2022-05-11 22:45:27 +0800

    添加注释

--- src/main/java/com/wuwu/base/client/first/ProtocolUtil.java
+++ src/main/java/com/wuwu/base/client/first/ProtocolUtil.java
@@ -1,6 +1,7 @@
 package com.wuwu.base.client.first;
 
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -27,18 +28,20 @@ public class ProtocolUtil {
 
         String[] args = common.split(" ");
 
-        StringBuffer sb = new StringBuffer();
-        sb.append("*").append(args.length).append(CRLF);
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("*").append(args.length).append(CRLF);
 
         for (String arg : args) {
-            sb.append("$")
-                    .append(arg.length())
-                    .append(CRLF)
-                    .append(arg)
-                    .append(CRLF);
+            if (!arg.equals(" ")) {
+                buffer.append("$")
+                        .append(arg.length())
+                        .append(CRLF)
+                        .append(arg)
+                        .append(CRLF);
+            }
         }
 
-        return StandardCharsets.UTF_8.encode(sb.toString());
+        return StandardCharsets.UTF_8.encode(buffer.toString());
 
     }
 
@@ -50,7 +53,12 @@ public class ProtocolUtil {
      */
     public static void decode(WuwuFutureClient futureClient) {
         // TODO 最难写的地方就在这了，
+        SocketChannel socketChannel = futureClient.getSocketChannel();
 
+        // TODO 1.抽象出每一种类型单独的解析对象的方法，并返回解析结果
+        //  如果解析完成，则直接返回
+        //  如果解析未完成，则需要记录未完成，并记录上一次的解析位置，一直已经完成的解析内容，下一次再进来的时候，先判断是否完成解析，
+        //  如果没有，从上次未解析的地方解析
 
 
     }
